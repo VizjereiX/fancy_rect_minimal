a642532c2a20a382c797c29e69179f80beffe726
Marcin Szajek <marcin.szajek@gmail.com>
2025-11-10 23:38:43 +0100

    Short circuiting == + \n jako newlines (234)

--- fancy_rect.py
+++ fancy_rect.py
@@ -1,3 +1,3 @@
-import sys as s;v=s.argv;len(v)==3 or s.exit(f"usage: {v[0]} <width> <height>")
+import sys as s;v=s.argv;len(v)!=3==s.exit(f"usage: {v[0]} <width> <height>")
 def fancy_rect(w,h):c,x,m,n=chr,9552,w-2,"\n";a,b=c(x),c(x+1);print(c(x+4)+a*m+c(x+7)+n+(b+" "*m+b+n)*(h-2)+c(x+10)+a*m+c(x+13))
 fancy_rect(*map(int,v[1:]))
