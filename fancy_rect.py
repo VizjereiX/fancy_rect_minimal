9bde086503d2b34e1fc0b11f1961208fe2dbad04
Marcin Szajek <marcin.szajek@gmail.com>
2025-11-10 23:17:40 +0100

    rozpakowywanie listy + map! (240)

--- fancy_rect.py
+++ fancy_rect.py
@@ -1,3 +1,4 @@
 import sys as s
 def fancy_rect(w,h):c,x,m,n=chr,9552,w-2,"\n";a,b=c(x),c(x+1);print(c(x+4)+a*m+c(x+7)+n+(b+" "*m+b+n)*(h-2)+c(x+10)+a*m+c(x+13))
-v=s.argv;len(v)==3 or s.exit(f"usage: {v[0]} <width> <height>");fancy_rect(int(v[1]),int(v[2]))
+v=s.argv;len(v)==3 or s.exit(f"usage: {v[0]} <width> <height>")
+fancy_rect(*map(int, v[1:]))
