732a37765727dcf288ce9fe394f4cd8eba0c94f0
Marcin Szajek <marcin.szajek@gmail.com>
2025-11-10 22:55:08 +0100

    print is shorter, and we are using it ones (264)

--- fancy_rect.py
+++ fancy_rect.py
@@ -2,7 +2,7 @@ import sys as s
 def fancy_rect(w,h):
  c,x,m,n=chr,0x2550,w-2,"\n"
  a,b=c(x),c(x+1)
- s.stdout.write(c(x+4)+a*m+c(x+7)+n+(b+" "*m+b+n)*(h-2)+c(x+10)+a*m+c(x+13)+n)
+ print(c(x+4)+a*m+c(x+7)+n+(b+" "*m+b+n)*(h-2)+c(x+10)+a*m+c(x+13))
 v=s.argv           
 len(v)==3 or s.exit(f"usage: {v[0]} <width> <height>")
 fancy_rect(int(v[1]),int(v[2]))
