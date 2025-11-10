4502050e3074fd44c9a8c64f203118d2e12a7da4
Marcin Szajek <marcin.szajek@gmail.com>
2025-11-10 22:42:04 +0100

    single write? why not (275)

--- fancy_rect.py
+++ fancy_rect.py
@@ -2,10 +2,7 @@ import sys as s
 def fancy_rect(w,h):
  c,x,m,n=chr,0x2550,w-2,"\n"
  a,b=c(x),c(x+1)
- def p(*r): s.stdout.write("".join(r))
- p(c(x+4),a*m,c(x+7),n)
- p((b+" "*m+b+n)*(h-2))
- p(c(x+10),a*m,c(x+13)+n)
+ s.stdout.write(c(x+4)+a*m+c(x+7)+n+(b+" "*m+b+n)*(h-2)+c(x+10)+a*m+c(x+13)+n)
 v=s.argv           
 len(v)==3 or s.exit(f"usage: {v[0]} <width> <height>")
 fancy_rect(int(v[1]),int(v[2]))
