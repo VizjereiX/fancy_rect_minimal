5a786b3957a94bd09ebbb2835b33ae41aab747f8
Marcin Szajek <marcin.szajek@gmail.com>
2025-11-11 23:41:09 +0100

    fancy chars are must have, so I need to rollback that, but I can use -= trick to shave a bit more bytes (135)

--- fancy_rect.py
+++ fancy_rect.py
@@ -1,4 +1,4 @@
 import sys
-def f(w,h):m=w-2;a="="*m;print(f"╔{a}╗\n{('|'+' '*m+'|\n')*(h-2)}╚{a}╝")
+def f(w,h):w-=2;a="═"*w;print(f"╔{a}╗\n{('║'+' '*w+'║\n')*(h-2)}╚{a}╝")
 fancy_rect=f
 f(*map(int,sys.argv[1:]))
